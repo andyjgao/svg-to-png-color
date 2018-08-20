3cd099196dd4d3d0ba1c08c2302dbe9773a92a66
Andy Gao <agao@andrew.cmu.edu>
2018-08-20 13:02:56 -0400

    added comments into code

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# -------------------------------------------------------------------------------
+# Author: Chase Midler, Andy Gao
+# -------------------------------------------------------------------------------
+# Tools uses:
+# Flask,Python
+# -------------------------------------------------------------------------------
 from flask import Flask, send_file, request, after_this_request, render_template
 import requests
 import os
@@ -7,12 +13,21 @@ app = Flask(__name__)
 api = Api(app)
 
 
+# @route GET /
+# @desc Read me page on how to use API
+# @access Public
 @app.route('/')
 def read_me():
     return render_template('readme.html')
 
+
+# @route GET /image?
+# @desc Given an RGB, displays a 200 x 200 px of the color in png for temp use, then deletes image after request
+# @access Public
 class SvgToPng(Resource):
     def __init__(self):
+
+        # Initialized arguments that can be added
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('R', type=str, location='args')
         self.reqparse.add_argument('G', type=str, location='args')
@@ -20,13 +35,20 @@ class SvgToPng(Resource):
         super().__init__()
 
     def get(self):
+        
+        # adds arguments from url to list 
         argslist = self.reqparse.parse_args()
 
+        # sends request for svg color of the image 
         r = requests.get('http://www.thecolorapi.com/id?rgb={},{},{}&w=150&h=150'.format(argslist['R'],argslist['G'],argslist['B']))
         response = r.json()
         image_url = response['image']['bare']
+        
+        # converts svg image into png 
         cairosvg.svg2png(
             url= image_url, write_to='image.png', scale= 2)
+        
+        # deletes image after request
         @after_this_request
         def remove_file(response):
             os.remove('./image.png')
